0ab2284dbc5df1f2a78107bd1abc97cb42b405e7
Shawn Cochran <shawn.cochran31@gmail.com>
2021-08-22 22:10:57 -0400

    service worker implemented in to code

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,7 +10,7 @@ request.onupgradeneeded = function (event) {
 request.onsuccess = function (event) {
     db = event.target.result;
     if (navigator.onLine) {
-        uploadTransaction();
+        // uploadTransaction();
     }
 };
 
